d18a1613664a11de65b04478e78aff229bebd392
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-04-01 16:36:54 +0200

    move to widukind-common

--- widukind_api/wsgi.py
+++ widukind_api/wsgi.py
@@ -124,7 +124,7 @@ def _conf_logging(debug=False,
 
 def _conf_logging_mongo(app):
     #TODO: distinction entre app web et api dans collection
-    from widukind_api.mongo_logging_handler import MongoHandler
+    from widukind_common.mongo_logging_handler import MongoHandler
     handler = MongoHandler(db=app.widukind_db, collection=constants.COL_LOGS)
     handler.setLevel(logging.ERROR)
     app.logger.addHandler(handler)
@@ -168,10 +168,10 @@ def _conf_db(app):
     app.widukind_db = get_mongo_db(app.config.get("MONGODB_URL"))
 
 def _conf_session(app):
-    from widukind_api.mongo_session import PyMongoSessionInterface
+    from widukind_common.flask_utils.mongo_session import PyMongoSessionInterface
     app.session_interface = PyMongoSessionInterface(app.widukind_db,
                                                     collection=constants.COL_SESSION)
-    
+        
 def _conf_cache(app):
     """
     @cache.cached(timeout=50)
