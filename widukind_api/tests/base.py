0401041d5cffc020fe9e1f0a24a0fdeffbf1b7f8
Stéphane RAULT <stephane.rault@radicalspam.org>
2016-06-30 19:20:15 +0200

    Refactoring tests

--- widukind_api/tests/base.py
+++ widukind_api/tests/base.py
@@ -1,20 +1,75 @@
 # -*- coding: utf-8 -*-
 
-from flask_testing import TestCase as BaseTestCase
+import os
 
-from widukind_common.utils import create_or_update_indexes
 from widukind_common import tests_tools as utils
 
-class TestCase(BaseTestCase):
+from widukind_common.tests.flask_base_tests import BaseFlaskTestCase 
+
+from widukind_api import constants
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+RESOURCES_DIR = os.path.abspath(os.path.join(BASE_DIR, "resources"))
+
+REFERENTIEL = {
+    "providers": {
+        "count": 1,
+    },
+    "categories": {
+        "count": 226,
+    },
+    "datasets": {
+        "count": 1,
+    },
+    "series": {
+        "count": 100,
+    },
+    "tags": {
+        "count": 51,
+    },               
+}
+
+def create_fixtures(db):
+    """
+    mongoexport --host mongodb --pretty --jsonArray --db widukind --collection providers -q "{name: 'INSEE'}" | gzip -c >/widukind/providers.json.gz
+    mongoexport --host mongodb --pretty --jsonArray --db widukind --collection categories -q "{provider_name: 'INSEE'}" | gzip -c >/widukind/categories.json.gz
+    mongoexport --host mongodb --pretty --jsonArray --db widukind --collection datasets -q "{ dataset_code: 'IPCH-2015-FR-COICOP'}" | gzip -c >/widukind/datasets.json.gz
+    mongoexport --host mongodb --pretty --jsonArray --db widukind --collection series -q "{provider_name: 'INSEE', dataset_code: 'IPCH-2015-FR-COICOP'}" --limit 100 | gzip -c >/widukind/series.json.gz
+    mongoexport --host mongodb --pretty --jsonArray --db widukind --collection tags -q '{provider_name: {"$in": ["INSEE"]}, count: { "$gte": 50} }' | gzip -c >/widukind/tags.json.gz
+    """
+    import gzip
+    from bson import json_util
+    filenames = {
+        "providers": os.path.abspath(os.path.join(RESOURCES_DIR, "providers.json.gz")),
+        "categories": os.path.abspath(os.path.join(RESOURCES_DIR, "categories.json.gz")),
+        "datasets": os.path.abspath(os.path.join(RESOURCES_DIR, "datasets.json.gz")),
+        "series": os.path.abspath(os.path.join(RESOURCES_DIR, "series.json.gz")),
+        "tags": os.path.abspath(os.path.join(RESOURCES_DIR, "tags.json.gz")),
+    }
+    for collection, filepath in filenames.items():
+        #print("filepath : ", filepath)
+        datas = json_util.loads(gzip.open(filepath).read().decode())
+        if not isinstance(datas, list):
+            datas = [datas]
+        for d in datas:
+            if 'metadata' in d:
+                d['metadata'] = {}
+        result = db[collection].insert_many(datas)
+        #print(collection, " : ", len(result.inserted_ids))
+            
+class TestCase(BaseFlaskTestCase):
     
     def setUp(self):
         super().setUp()
-        self.db = self.app.widukind_db
         self.assertEqual(self.db.name, "widukind_test")
-        utils.clean_mongodb(self.db)
-        create_or_update_indexes(self.db, force_mode=True)
 
-    def create_app(self):
+    def _create_app(self):
         from widukind_api import wsgi
-        app = wsgi.create_app('widukind_api.settings.Test')
+        app = wsgi.create_app('widukind_api.settings.Test', db=self.db)
         return app
+    
+    def fixtures(self):
+        create_fixtures(self.db)
+    
+    def clean_db(self):
+        utils.clean_mongodb(collection_list=constants.COL_ALL, db=self.db)
--- widukind_api/tests/test_views.py
+++ widukind_api/tests/test_views.py
@@ -1,31 +1,62 @@
 # -*- coding: utf-8 -*-
 
+import unittest
+from datetime import datetime
 from flask import url_for
 
-from widukind_api.tests.base import TestCase
+from widukind_api.tests.base import TestCase, REFERENTIEL
 
-class ViewsTestCase(TestCase):
+from pprint import pprint
+
+class JsonPluginTestCase(TestCase):
     
-    # nosetests -s -v widukind_api
+    # nosetests -s -v widukind_api.tests.test_views:JsonPluginTestCase
+    
+    def setUp(self):
+        super().setUp()
+        self.fixtures()
+        
+    def test_views_providers_list(self):
+        response = self.json_get(url_for('json.providers-list'))
+        self.assertOkJson(response)
+        response_json = self.response_json(response)
+        self.assertEquals(len(response_json["data"]), REFERENTIEL["providers"]["count"])
+        pprint(response_json)
+        self.assertEquals(response_json["data"], 
+                          [{'long_name': 'National Institute of Statistics and Economic Studies',
+                            'name': 'INSEE',
+                            'region': 'France',
+                            'slug': 'insee',
+                            'website': 'http://www.insee.fr'}])
     
     """
-    TODO:
-    /                              home HEAD,OPTIONS,GET
-    /EVIEWS/<provider>/dataset/<dataset_code>/values EVIEWS.EVIEWS_query_series POST,HEAD,OPTIONS,GET
-    /api/v1/<provider>             COMMON.get_provider HEAD,OPTIONS,GET
-    /api/v1/<provider>/categories  COMMON.get_categories HEAD,OPTIONS,GET
-    /api/v1/<provider>/dataset/<dataset_code> COMMON.get_dataset HEAD,OPTIONS,GET
-    /api/v1/<provider>/dataset/<dataset_code>/series COMMON.get_series HEAD,OPTIONS,GET
-    /api/v1/<provider>/dataset/<dataset_code>/series/<key> COMMON.get_a_series HEAD,OPTIONS,GET
-    /api/v1/<provider>/dataset/<dataset_code>/values COMMON.get_values HEAD,OPTIONS,GET
-    /api/v1/providers              COMMON.get_providers HEAD,OPTIONS,GET
-    /static/<path:filename>        static HEAD,OPTIONS,GET        
+    providers_list_keys
+    
+    def test_views_ajax_datasets_list(self):
+        response = self.json_get(url_for('views.ajax-datasets-list', provider="NOTEXIST"))
+        self.assert_404(response)
+        
+        response = self.json_get(url_for('views.ajax-datasets-list', provider="insee"))
+        self.assertOkJson(response)
+        response_json = self.response_json(response)
+        self.assertEquals(len(response_json["data"]), REFERENTIEL["datasets"]["count"])
+        self.assertEquals(response_json["data"], 
+                          [{'dataset_code': 'IPCH-2015-FR-COICOP', 
+                            'name': 'Harmonised consumer price index - Base 2015 - France - By product (COICOP classification)', 
+                            'slug': 'insee-ipch-2015-fr-coicop',
+                            'enable': True}])
     """
-        
-    def test_urls(self):
-        url = url_for("home")
-        response = self.client.get(url)        
-        self.assert200(response)
-        self.assert_template_used('index.html')
-        
-        
+    
+class HomeViewsTestCase(TestCase):
+
+    # nosetests -s -v widukind_api.tests.test_views:HomeViewsTestCase
+
+    def setUp(self):
+        super().setUp()
+
+    def test_views_index(self):
+        response = self.client.get(url_for('home'))
+        self.assertOkHtml(response)
+        self.assert_template_used("index.html")
+
+
